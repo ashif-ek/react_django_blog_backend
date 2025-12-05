fd04632135bd99c56502cca00241ff6b3d78b05d
Ashif EK <ashifek11@gmail.com>
2025-12-05 10:51:48 +0530

    Add author field to Post model and update PostSerializer

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,21 @@
 from django.db import models
 from django.utils.text import slugify
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(unique=True, blank=True)
     content = models.TextField()
+
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="posts",
+    )
+
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
@@ -13,7 +24,17 @@ class Post(models.Model):
 
     def save(self, *args, **kwargs):
         if not self.slug:
-            self.slug = slugify(self.title)
+            # avoid slug collisions: "My Post", "My Post" -> "my-post", "my-post-1"
+            base_slug = slugify(self.title)
+            slug = base_slug
+            counter = 1
+
+            while Post.objects.filter(slug=slug).exists():
+                slug = f"{base_slug}-{counter}"
+                counter += 1
+
+            self.slug = slug
+
         super().save(*args, **kwargs)
 
     def __str__(self):
--- blog/serializers.py
+++ blog/serializers.py
@@ -1,7 +1,22 @@
 from rest_framework import serializers
 from .models import Post
 
+
 class PostSerializer(serializers.ModelSerializer):
+    author_username = serializers.CharField(
+        source="author.username", read_only=True
+    )
+
     class Meta:
         model = Post
-        fields = ['id', 'title', 'slug', 'content', 'created', 'updated']
+        fields = [
+            "id",
+            "title",
+            "slug",
+            "content",
+            "author",           # PK
+            "author_username",  # nice to show in UI
+            "created",
+            "updated",
+        ]
+        read_only_fields = ["author", "slug", "created", "updated"]
